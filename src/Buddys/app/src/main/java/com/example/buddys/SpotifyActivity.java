57cc34b712ca4ab23e221f150671e40904ce2e50
Josefrrrd3 <jose.frrrd3@eduge.ch>
2021-11-10 13:37:16 +0100

    SpotifyActivity

---
+++ src/Buddys/app/src/main/java/com/example/buddys/SpotifyActivity.java
@@ -0,0 +1,14 @@
+package com.example.buddys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify);
+    }
+}
