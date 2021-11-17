3e593f51ab2493b67f51c96ea497f64320183eae
Josefrrrd3 <jose.frrrd3@eduge.ch>
2021-11-17 15:21:02 +0100

    settings

--- src/TripBuddy/app/src/main/java/com/example/tripbuddy/MainActivity.java
+++ src/TripBuddy/app/src/main/java/com/example/tripbuddy/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.tripbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.Menu;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+    }
+    public  void openSettings(){
+        Intent intent = new Intent(this, Preference.class);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        Intent i = new Intent (this,Preference.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
