95663b0fbab601cfc100285825a7abd503edcd85
Josefrrrd3 <jose.frrrd3@eduge.ch>
2021-11-24 15:12:16 +0100

    bruhleprofil

--- src/TripBuddy/app/src/main/java/com/example/tripbuddy/HomeActivity.java
+++ src/TripBuddy/app/src/main/java/com/example/tripbuddy/HomeActivity.java
@@ -18,11 +18,18 @@ import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     private Button button;
+    private Button btnProf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        btnProf = (Button) findViewById(R.id.btnProfil);
+        btnProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUser();
+            }
+        });
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +43,9 @@ public class HomeActivity extends AppCompatActivity {
     public  void openSettings(){
         Intent intent = new Intent(this, Preference.class);
         startActivity(intent);
+    }
+    public  void openUser(){
+        Intent Intent2 = new Intent(this,UserActivity.class);
+        startActivity(Intent2);
     }
 }
---
+++ src/TripBuddy/app/src/main/java/com/example/tripbuddy/UserActivity.java
@@ -0,0 +1,38 @@
+package com.example.tripbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.Menu;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class UserActivity extends AppCompatActivity {
+    private Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        btnBack = (Button) findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+    }
+    public  void openHome(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
