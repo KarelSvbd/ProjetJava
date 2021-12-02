db3aa918b9fdd05ed9682cb49de6a0f1cb502185
KarelSvbd <57522089+KarelSvbd@users.noreply.github.com>
2021-12-02 11:31:48 +0100

    oui

--- src/TripBuddy/app/src/main/java/com/example/tripbuddy/ModeR.java
+++ src/TripBuddy/app/src/main/java/com/example/tripbuddy/ModeR.java
@@ -1,12 +1,21 @@
 package com.example.tripbuddy;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,25 +30,48 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ModeR extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, LocationListener {
 
-
+    TextView lblSpeed;
+    private GoogleMap map;
+    private LocationManager locationManager;
     public static Location _location;
+    private Marker markerUser;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moder);
+        lblSpeed = findViewById(R.id.lblVitesse);
+        lblSpeed.setText("0");
 
+        getLocation();
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        _location = location;
+        markerUser.remove();
+        markerUser = map.addMarker(new MarkerOptions()
+                .position(new LatLng(_location.getLatitude(), _location.getLongitude()))
+                .title("Votre position")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(), R.drawable.ic_baseline_arrow_drop_down_circle_24)));
         TextView lblSpeed = findViewById(R.id.lblSpeed);
+        _location = location;
+        System.out.println("test");
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 20));
         lblSpeed.setText(String.valueOf(Math.round(location.getSpeed() * 3.6)));
+
     }
 
     @Override
@@ -54,6 +86,36 @@ public class ModeR extends AppCompatActivity implements GoogleMap.OnMyLocationBu
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(_location.getLatitude(), _location.getLongitude()), 5));
+        map = googleMap;
 
+        markerUser = map.addMarker(new MarkerOptions()
+                .position(new LatLng(1,1))
+                .title("Chargement"));
+
+    }
+
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try{
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, ModeR.this);
+        }catch (Exception e){
+            System.out.print(e);
+        }
+    }
+
+    //Permet de chercher le Bitmap afin de changer l'icone de l'utilisateur
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, Integer vectorResId){
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0,0,vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap= Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap((bitmap));
     }
 }
